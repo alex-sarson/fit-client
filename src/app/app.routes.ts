c1303449cf9d1bbd73a4efde74e2aee7d3f85d27
alex-sarson <alex.luke.sarson@gmail.com>
2024-05-06 20:42:46 +0100

    added component routing, header & footer component init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import HomeComponent from './pages/home/home.component';
+import LoginComponent from './pages/login/login.component';
+import RegisterComponent from './pages/register/register.component';
+import ForgetPasswordComponent from './pages/forget-password/forget-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => HomeComponent },
+  { path: 'home', loadComponent: () => HomeComponent },
+  { path: 'login', loadComponent: () => LoginComponent },
+  { path: 'register', loadComponent: () => RegisterComponent },
+  { path: 'forgotten-password', loadComponent: () => ForgetPasswordComponent },
+];
